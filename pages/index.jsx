2af830ab18845a8f969e9cef7c379123a1546381
c1handev <chnkargo@gmail.com>
2024-08-05 01:20:15 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -86,7 +86,7 @@ CiBot'a Oy Ver(daha gelmedi)</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://cdn.discordapp.com/attachments/1268641535424200829/1269781697386250362/WhatsApp_Image_2024-08-05_at_01.16.44.jpeg?ex=66b14fdc&is=66affe5c&hm=2acc2e877319f17ab520b4f8591969dbc4f6cb012e03e4347622f8f0cdaba2a2&"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">En İyi Bot!</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">CiBot İle Yeni Bir Maceraya Çıkmaya Hazırmısın?</p>
                 </div>
